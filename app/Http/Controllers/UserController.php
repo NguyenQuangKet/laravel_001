ac70a2184eb658b0ade552d94f41a9d0d8dbcd2c
Tommy Nguyen <ket.cuc.it.ptit@gmail.com>
2019-12-06 12:01:10 +0700

    login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\LoginRequest;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 class UserController extends Controller
 {
     public  function getLogin()
@@ -13,7 +13,18 @@ class UserController extends Controller
     }
 
     public function postLogin(LoginRequest $request) {
+        $email = $request->email;
+        $password = $request->password;
 
+        $user = DB::table('users')->where(['email' => $email])->get();
+        if(!$user){
+            dd('dntb');
+        }else{
+            if($user[0]->password == $password){
+                dd('dntc');
+            }
+            dd('mks');
+        }
     }
 
 }
